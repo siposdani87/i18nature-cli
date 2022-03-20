1abe3609b16991305fb556b839a3e861dc23b065
Dániel Sipos <dsipos@szintezis-net.hu>
2022-03-20 20:12:07 +0100

    Create build

--- dist/lib/task.d.ts
+++ dist/lib/task.d.ts
@@ -1,5 +1,5 @@
-import Listr from 'listr';
+import { ListrTask } from 'listr';
 import { FileInfo, TranslationFile } from './model';
-export declare const getUploadTasksFromTranslationFiles: (translationFiles: TranslationFile[], taskCallback: (translationFile: TranslationFile, fileInfo: FileInfo) => Promise<void>) => Listr.ListrTask<any>[];
-export declare const getDownloadTasksFromTranslationFiles: (translationFiles: TranslationFile[], taskCallback: (translationFile: TranslationFile, fileInfo: FileInfo) => Promise<void>) => Listr.ListrTask<any>[];
-export declare const runTasks: (taskList: Listr.ListrTask<any>[]) => Promise<void>;
+export declare const getUploadTasksFromTranslationFiles: (translationFiles: TranslationFile[], taskCallback: (translationFile: TranslationFile, fileInfo: FileInfo) => Promise<void>) => ListrTask<any>[];
+export declare const getDownloadTasksFromTranslationFiles: (translationFiles: TranslationFile[], taskCallback: (translationFile: TranslationFile, fileInfo: FileInfo) => Promise<void>) => ListrTask<any>[];
+export declare const runTasks: (taskList: ListrTask<any>[]) => Promise<void>;
