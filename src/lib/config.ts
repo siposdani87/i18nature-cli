03a28144a1774f35b951594f2eca03f758e876c6
Dániel Sipos <siposdani87@hotmail.com>
2023-07-02 11:29:48 +0200

    Set new debug base url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -6,7 +6,7 @@ export const FILENAME = '.i18naturerc.json';
 export const DEBUG_FILENAME = '.i18naturerc-debug.json';
 export const CURRENT_WORK_DIR = process.cwd();
 
-export const DEBUG_BASE_URL = 'http://localhost';
+export const DEBUG_BASE_URL = 'http://0.0.0.0:80';
 export const BASE_URL = 'https://app.i18nature.com';
 
 export const GREEN_COLOR = '#0A4414';
